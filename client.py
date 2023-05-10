24e10218f2b8c16c2e2dab1862c3688237367759
JGD2108 <gomezdelahoz@uninorte.edu.co>
2023-05-10 11:45:20 -0500

    primer commit

--- client.py
+++ client.py
@@ -2,28 +2,34 @@ import socket
 import pickle
 import numpy as np
 
-HOST1 = '10.20.46.60'  # Replace with the IP address of server 1
-PORT1 = 9090
+HOST = '192.168.47.199'  # Replace with the IP address of the server
+PORT = 49152
 
 # Generate some sample data for the regression
 X = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])
 y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
 
-params = {'X': X, 'y': y}
+# Set hyperparameters
+hyperparameters = {'X': X, 'y': y}
 
+# Create a socket object
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     try:
-        s.connect((HOST1, PORT1))
-        data = pickle.dumps(params)
+        # Connect to the server
+        s.connect((HOST, PORT))
+        
+        # Send hyperparameters to the server
+        data = pickle.dumps(hyperparameters)
         s.sendall(data)
-        response = b''
-        while True:
-            chunk = s.recv(1024)
-            if not chunk:
-                break
-            response += chunk
+        
+        # Receive the trained model from the server
+        response = s.recv(1024)
         model = pickle.loads(response)
-        print(model.coef_)
+        
+        # Use the trained model for predictions or further processing
+        y_pred = model.predict(X)
+        print("Predicted values:", y_pred)
+        
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
--- server1.py
+++ server1.py
@@ -2,32 +2,43 @@ import socket
 import pickle
 from sklearn.linear_model import LinearRegression
 
-HOST1 = '10.20.46.60'  # Replace with the IP address of server 1
-PORT1 = 9090
+HOST = '192.168.47.199'  # Replace with the IP address of the server
+PORT = 49152
 
-# Create a socket object
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Bind the socket to a specific address and port
-    s.bind((HOST1, PORT1))
-    # Listen for incoming connections
-    s.listen()
-    print(f"Server 1 listening on {HOST1}:{PORT1}")
-    while True:
-        # Wait for a connection
-        conn, addr = s.accept()
-        with conn:
+def handle_client(conn):
+    # Receive hyperparameters from the client
+    data = conn.recv(1024)
+    hyperparameters = pickle.loads(data)
+    
+    # Extract hyperparameters
+    X = hyperparameters['X']
+    y = hyperparameters['y']
+    
+    # Fit a linear regression model to the data
+    model = LinearRegression().fit(X, y)
+    
+    # Send the trained model back to the client
+    response = pickle.dumps(model)
+    conn.sendall(response)
+    
+    # Close the connection
+    conn.close()
+
+def start_server():
+    # Create a socket object
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Bind the socket to a specific address and port
+        s.bind((HOST, PORT))
+        # Listen for incoming connections
+        s.listen()
+        print(f"Server listening on {HOST}:{PORT}")
+        while True:
+            # Wait for a connection
+            conn, addr = s.accept()
             print(f"Connected by {addr}")
-            data = b''
-            chunk = conn.recv(1024)
-            data += chunk
-            print(data)
-                
-            params = pickle.loads(data)
-            X, y = params['X'], params['y']
-            # Fit a linear regression model to the data
-            model = LinearRegression().fit(X, y)
-            response = pickle.dumps(model)
-            conn.sendall(response)
+            
+            # Handle the client request in a separate thread or process
+            handle_client(conn)
 
-
+start_server()
 
